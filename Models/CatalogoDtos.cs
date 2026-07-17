bce22b13d0484bd7c9186b9191fe073fa43a5467
William Escobar <wescobarr@gmail.com>
2026-07-16 19:13:44 -0500

    ajuste del popup

--- Models/CatalogoDtos.cs
+++ Models/CatalogoDtos.cs
@@ -39,5 +39,22 @@ namespace TiempoBiblia.Client.Models
         // Relaciones Múltiples
         public List<ProductoCategoriaSecundariaDto> CategoriasSecundarias { get; set; } = new();
         public List<ProductoTagDto> ProductoTags { get; set; } = new();
+        public List<ProductoRelacionadoDto> ProductosRelacionadosOrigen { get; set; } = new();
+    }
+    // 1. NUEVO: DTO para el Paquete
+    public class PaqueteDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public string ImagenUrl { get; set; } = string.Empty;
+    }
+    // 2. NUEVO: DTO para los Productos Relacionados
+    public class ProductoRelacionadoDto
+    {
+        public int ProductoRelacionadoId { get; set; }
+        // Traemos el producto destino para poder dibujar su tarjeta en el Pop-up
+        public ProductoDto ProductoRelacionadoDestino { get; set; } = new(); 
     }
 }
--- obj/Debug/net10.0/TiempoBiblia.Client.AssemblyInfo.cs
+++ obj/Debug/net10.0/TiempoBiblia.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiempoBiblia.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee489942c4657d7be72c4cd766627e249638f389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6914ef2fa280332581ce6a39ff876dcae1d22687")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiempoBiblia.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiempoBiblia.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
